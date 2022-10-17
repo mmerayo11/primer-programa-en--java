a4391508a581e04534154bfa07a67c2befd543c6
Mario <mmerayo1803@gmail.com>
2022-10-17 08:51:17 +0200

    sin apellidos y con cantante favorito

--- Prueba.java
+++ Prueba.java
@@ -2,9 +2,9 @@ public class Prueba {
 
 	public static void main(String args[]) {
 
-		System.out.println("Merayo Barredo");
 		System.out.println("19 años 2003");
 		System.out.println("Lasaña");
+		System.out.println("Feid");
 
 	}
 
